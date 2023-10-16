ad2eba12c214b645f5155c3f6e0aced7337226f4
David <david.diax66@gmail.com>
2023-10-16 20:32:30 +0100

    a

--- src/main/java/dad/calculadoracompleja/Calculadora.java
+++ src/main/java/dad/calculadoracompleja/Calculadora.java
@@ -1,11 +1,18 @@
 package dad.calculadoracompleja;
 
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
 public class Calculadora extends Application {
 
@@ -13,34 +20,87 @@ public class Calculadora extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		
 		// ui
-			HBox root;
-			
-			
-			HBox operando1HBox;
-			HBox operando2HBox;
-			HBox resultadoHBox;
-			
-			ComboBox<String> operadorCombo = new ComboBox<>();
-			operadorCombo.getItems().addAll("+","-","*","/");
-			
-			VBox seleccionOperacionVBox = new VBox(operadorCombo);
-			
-			TextField num1Real = new TextField();
-			TextField num1Imaginario = new TextField();
-			TextField num2Real = new TextField();
-			TextField num2Imaginario = new TextField();
-			TextField resultadoReal = new TextField();
-			TextField resultadoImaginario = new TextField();
-			
-			VBox operacionesVBox = new VBox();
-			
-			root = new HBox(5, seleccionOperacionVBox, operacionesVBox);
-			operacionesVBox.getC
+		
+		ComboBox<String> operadorCombo = new ComboBox<>();
+		operadorCombo.getItems().addAll("+","-","*","/");
+		VBox cajaComboBox = new VBox(5, operadorCombo);
+		cajaComboBox.setAlignment(Pos.CENTER);
+		
+		
+
+		TextField num1Real = new TextField();
+		TextField num1Imaginario = new TextField();
+		HBox operando1 = new HBox(5, num1Real, new Label("+"), num1Imaginario, new Label("i"));
+		
+		
+		TextField num2Real = new TextField();
+		TextField num2Imaginario = new TextField();
+		HBox operando2 = new HBox(5, num2Real, new Label("+"), num2Imaginario, new Label("i"));
+		
+		TextField resultadoReal = new TextField();
+		TextField resultadoImaginario = new TextField();
+		HBox resultadoOperandos = new HBox(5, resultadoReal, new Label("+"), resultadoImaginario, new Label("i"));
+		
+		VBox cajaOperaciones = new VBox(5, operando1, operando2, new Separator(), resultadoOperandos);
+		cajaOperaciones.setAlignment(Pos.CENTER);
+		cajaOperaciones.setFillWidth(false);
+		
+		
+		HBox root = new HBox(5, cajaComboBox, cajaOperaciones);
+		root.setAlignment(Pos.CENTER);
+		
+		
+		Scene scene = new Scene(root, 420, 200);
+		
+		primaryStage.setTitle("Calculadora de números complejos");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 			
 			
 		// bindings
 		
+		DoubleProperty a = new SimpleDoubleProperty();	
+		DoubleProperty b = new SimpleDoubleProperty();
+		DoubleProperty c = new SimpleDoubleProperty();	
+		DoubleProperty d = new SimpleDoubleProperty();
+		DoubleProperty resultReal = new SimpleDoubleProperty();	
+		DoubleProperty resultImaginario = new SimpleDoubleProperty();
+		
+		num1Real.textProperty().bindBidirectional(a, new NumberStringConverter());
+		num1Imaginario.textProperty().bindBidirectional(b, new NumberStringConverter());
+		num2Real.textProperty().bindBidirectional(c, new NumberStringConverter());
+		num2Imaginario.textProperty().bindBidirectional(d, new NumberStringConverter());
+		resultadoReal.textProperty().bind(resultReal.asString());
+		resultadoImaginario.textProperty().bind(resultImaginario.asString());
+		
+		
 		// listener
+		
+		operadorCombo.getSelectionModel().selectedItemProperty().addListener((o, ov, nv) -> {
+			
+			switch(nv) {
+			case "+":
+				resultReal.bind(a.add(c));
+				resultImaginario.bind(b.add(d));
+				break;
+			case "-":
+				resultReal.bind(a.subtract(c));
+				resultImaginario.bind(b.subtract(d));
+				break;
+			case "*":
+				resultReal.bind(a.multiply(c).subtract(b.multiply(d)));
+				resultImaginario.bind(a.multiply(d).add(b.multiply(c)));
+				break;
+			case "/":
+				resultReal.bind(a.multiply(c).add(b.multiply(d))
+						.divide(c.multiply(c).add(d.multiply(d))));
+				resultImaginario.bind(b.multiply(c).subtract(a.multiply(d))
+						.divide(c.multiply(c).add(d.multiply(d))));
+			}
+		});
+		
+		operadorCombo.getSelectionModel().selectFirst();
 		
 	}
 
---
+++ src/main/java/dad/calculadoracompleja/Main.java
@@ -0,0 +1,13 @@
+package dad.calculadoracompleja;
+
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Application.launch(Calculadora.class, args);
+
+	}
+
+}
